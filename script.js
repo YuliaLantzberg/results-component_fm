43593c2947274ba2104bf1b08e058a1306705940
YuliaLantzberg <lanzyuka@gmail.com>
2024-09-01 16:07:37 +0300

    Add summary details dynamically

---
+++ script.js
@@ -0,0 +1,24 @@
+const categories = document.getElementsByClassName("category");
+
+fetch("/data.json")
+	.then((res) => res.json())
+	.then((dataJson) => {
+		for (let i = 0; i < categories.length; i++) {
+			const categoryName = document.createElement("div");
+			categoryName.classList.add("category__name");
+			const icon = document.createElement("img");
+			icon.src = dataJson[i].icon;
+			categoryName.appendChild(icon);
+			const name = document.createElement("p");
+			name.textContent = dataJson[i].category;
+			categoryName.appendChild(name);
+			const score = document.createElement("p");
+			score.classList.add("category__score");
+			const scoreNum = document.createElement("span");
+			scoreNum.textContent = dataJson[i].score;
+			score.appendChild(scoreNum);
+			scoreNum.after("/100");
+			categories[i].appendChild(categoryName);
+			categories[i].appendChild(score);
+		}
+	});
